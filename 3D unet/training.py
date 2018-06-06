c27bbb359663e645ca26948d876b1e0751816ea1
Lawrence <x15026471536@icloud.com>
2018-06-06 00:29:36 -0700

    cleaned

--- 3D unet/training.py
+++ 3D unet/training.py
@@ -5,58 +5,29 @@ from keras import backend as K
 from keras.callbacks import ModelCheckpoint, CSVLogger, LearningRateScheduler, ReduceLROnPlateau, EarlyStopping
 from keras.models import load_model
 
-def dice_coefficient(y_true, y_pred, smooth=1.):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+def train_model(model, model_file, training_generator, validation_generator, steps_per_epoch, validation_steps,
+                initial_learning_rate=0.001, learning_rate_drop=0.5, learning_rate_epochs=None, n_epochs=500,
+                learning_rate_patience=20, early_stopping_patience=None):
+    model.fit_generator(generator=training_generator,
+                        steps_per_epoch=steps_per_epoch,
+                        epochs=n_epochs,
+                        validation_data=validation_generator,
+                        validation_steps=validation_steps,
+                        callbacks=gettingCallbacks(model_file,
+                                                initial_learning_rate=initial_learning_rate,
+                                                learning_rate_drop=learning_rate_drop,
+                                                learning_rate_epochs=learning_rate_epochs,
+                                                learning_rate_patience=learning_rate_patience,
+                                                early_stopping_patience=early_stopping_patience))
 
-
-def dice_coefficient_loss(y_true, y_pred):
-    return -dice_coefficient(y_true, y_pred)
-
-
-def weighted_dice_coefficient(y_true, y_pred, axis=(-3, -2, -1), smooth=0.00001):
-    
-    return K.mean(2. * (K.sum(y_true * y_pred,
-                              axis=axis) + smooth/2)/(K.sum(y_true,
-                                                            axis=axis) + K.sum(y_pred,
-                                                                               axis=axis) + smooth))
-
-
-def weighted_dice_coefficient_loss(y_true, y_pred):
-    return -weighted_dice_coefficient(y_true, y_pred)
-
-
-def label_wise_dice_coefficient(y_true, y_pred, label_index):
-    return dice_coefficient(y_true[:, label_index], y_pred[:, label_index])
-
-
-def get_label_dice_coefficient_function(label_index):
-    f = partial(label_wise_dice_coefficient, label_index=label_index)
-    f.__setattr__('__name__', 'label_{0}_dice_coef'.format(label_index))
-    return f
-
-
-dice_coef = dice_coefficient
-dice_coef_loss = dice_coefficient_loss
-
-K.set_image_dim_ordering('th')
-
-
-
-def step_decay(epoch, initial_lrate, drop, epochs_drop):
-    return initial_lrate * math.pow(drop, math.floor((1+epoch)/float(epochs_drop)))
-
-
-def get_callbacks(model_file, initial_learning_rate=0.0001, learning_rate_drop=0.5, learning_rate_epochs=None,
+def gettingCallbacks(model_file, initial_learning_rate=0.0001, learning_rate_drop=0.5, learning_rate_epochs=None,
                   learning_rate_patience=50, logging_file="training.log", verbosity=1,
                   early_stopping_patience=None):
     callbacks = list()
     callbacks.append(ModelCheckpoint(model_file, save_best_only=True))
     callbacks.append(CSVLogger(logging_file, append=True))
     if learning_rate_epochs:
-        callbacks.append(LearningRateScheduler(partial(step_decay, initial_lrate=initial_learning_rate,
+        callbacks.append(LearningRateScheduler(partial(decaysteps, initial_lrate=initial_learning_rate,
                                                        drop=learning_rate_drop, epochs_drop=learning_rate_epochs)))
     else:
         callbacks.append(ReduceLROnPlateau(factor=learning_rate_drop, patience=learning_rate_patience,
@@ -65,13 +36,53 @@ def get_callbacks(model_file, initial_learning_rate=0.0001, learning_rate_drop=0
         callbacks.append(EarlyStopping(verbose=verbosity, patience=early_stopping_patience))
     return callbacks
 
+def coef_dice(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def coef_dice_loss(y_true, y_pred):
+    return -coef_dice(y_true, y_pred)
+
+
+def w_coef_dice(y_true, y_pred, axis=(-3, -2, -1), smooth=0.00001):
+    
+    return K.mean(2. * (K.sum(y_true * y_pred, axis=axis) + smooth/2)/(K.sum(y_true, axis=axis) + K.sum(y_pred, axis=axis) + smooth))
+
+
+def w_coef_dice_loss(y_true, y_pred):
+    return -w_coef_dice(y_true, y_pred)
+
+
+def label_wise_coef_dice(y_true, y_pred, label_index):
+    return coef_dice(y_true[:, label_index], y_pred[:, label_index])
+
+
+def get_label_coef_dice_function(label_index):
+    f = partial(label_wise_coef_dice, label_index=label_index)
+    f.__setattr__('__name__', 'label_{0}_dice_coef'.format(label_index))
+    return f
+
+
+dice_coef = coef_dice
+dice_coef_loss = coef_dice_loss
+
+K.set_image_dim_ordering('th')
+
+
+
+def decaysteps(epoch, initial_lrate, drop, epochs_drop):
+    return initial_lrate * math.pow(drop, math.floor((1+epoch)/float(epochs_drop)))
+
 
 def load_old_model(model_file):
     print("Loading pre-trained model")
-    custom_objects = {'dice_coefficient_loss': dice_coefficient_loss, 'dice_coefficient': dice_coefficient,
+    custom_objects = {'coef_dice_loss': coef_dice_loss, 'coef_dice': coef_dice,
                       'dice_coef': dice_coef, 'dice_coef_loss': dice_coef_loss,
-                      'weighted_dice_coefficient': weighted_dice_coefficient,
-                      'weighted_dice_coefficient_loss': weighted_dice_coefficient_loss}
+                      'w_coef_dice': w_coef_dice,
+                      'w_coef_dice_loss': w_coef_dice_loss}
     try:
         from keras_contrib.layers import InstanceNormalization
         custom_objects["InstanceNormalization"] = InstanceNormalization
@@ -86,18 +97,3 @@ def load_old_model(model_file):
         else:
             raise error
 
-
-def train_model(model, model_file, training_generator, validation_generator, steps_per_epoch, validation_steps,
-                initial_learning_rate=0.001, learning_rate_drop=0.5, learning_rate_epochs=None, n_epochs=500,
-                learning_rate_patience=20, early_stopping_patience=None):
-    model.fit_generator(generator=training_generator,
-                        steps_per_epoch=steps_per_epoch,
-                        epochs=n_epochs,
-                        validation_data=validation_generator,
-                        validation_steps=validation_steps,
-                        callbacks=get_callbacks(model_file,
-                                                initial_learning_rate=initial_learning_rate,
-                                                learning_rate_drop=learning_rate_drop,
-                                                learning_rate_epochs=learning_rate_epochs,
-                                                learning_rate_patience=learning_rate_patience,
-                                                early_stopping_patience=early_stopping_patience))
--- 3D unet/unet.py
+++ 3D unet/unet.py
@@ -1,64 +1,33 @@
 import numpy as np
 from functools import partial
-from keras import backend as K
 from keras.engine import Input, Model
 from keras.layers import Conv3D, MaxPooling3D, UpSampling3D, Activation, BatchNormalization, PReLU, Deconvolution3D
 from keras.optimizers import Adam
+from keras import backend as K
 
 
-def dice_coefficient(y_true, y_pred, smooth=1.):
+
+def coef_dice(y_true, y_pred, smooth=1.):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+def w_coef_dice_loss(y_true, y_pred):
+    return - w_coef_dice(y_true, y_pred)
 
-def dice_coefficient_loss(y_true, y_pred):
-    return -dice_coefficient(y_true, y_pred)
+def coef_dice_loss(y_true, y_pred):
+    return -coef_dice(y_true, y_pred)
 
-
-def weighted_dice_coefficient(y_true, y_pred, axis=(-3, -2, -1), smooth=0.00001):
-    """
-    Weighted dice coefficient. Default axis assumes a "channels first" data structure
-    :param smooth:
-    :param y_true:
-    :param y_pred:
-    :param axis:
-    :return:
-    """
-    return K.mean(2. * (K.sum(y_true * y_pred,
-                              axis=axis) + smooth/2)/(K.sum(y_true,
-                                                            axis=axis) + K.sum(y_pred,
-                                                                               axis=axis) + smooth))
-
-
-def weighted_dice_coefficient_loss(y_true, y_pred):
-    return -weighted_dice_coefficient(y_true, y_pred)
-
-
-def label_wise_dice_coefficient(y_true, y_pred, label_index):
-    return dice_coefficient(y_true[:, label_index], y_pred[:, label_index])
-
-
-def get_label_dice_coefficient_function(label_index):
-    f = partial(label_wise_dice_coefficient, label_index=label_index)
-    f.__setattr__('__name__', 'label_{0}_dice_coef'.format(label_index))
-    return f
-
-
-dice_coef = dice_coefficient
-dice_coef_loss = dice_coefficient_loss
-
-K.set_image_data_format("channels_first")
-
-try:
-    from keras.engine import merge
-except ImportError:
-    from keras.layers.merge import concatenate
+def w_coef_dice(y_true, y_pred, axis=(-3, -2, -1), smooth=0.00001):
+    return K.mean(2. * (K.sum(y_true * y_pred, axis=axis) + smooth/2)/(K.sum(y_true, axis=axis) + K.sum(y_pred, axis=axis) + smooth))
+    
+def label_wise_coef_dice(y_true, y_pred, label_index):
+    return coef_dice(y_true[:, label_index], y_pred[:, label_index])
 
 
 def unet_model_3d(input_shape, pool_size=(2, 2, 2), n_labels=1, initial_learning_rate=0.00001, deconvolution=False,
-                  depth=4, n_base_filters=32, include_label_wise_dice_coefficients=False, metrics=dice_coefficient,
+                  depth=4, n_base_filters=32, include_label_wise_coef_dices=False, metrics=coef_dice,
                   batch_normalization=False, activation_name="sigmoid"):
     inputs = Input(input_shape)
     current_layer = inputs
@@ -66,9 +35,9 @@ def unet_model_3d(input_shape, pool_size=(2, 2, 2), n_labels=1, initial_learning
 
 
     for layer_depth in range(depth):
-        layer1 = create_convolution_block(input_layer=current_layer, n_filters=n_base_filters*(2**layer_depth),
+        layer1 = setup_block(input_layer=current_layer, n_filters=n_base_filters*(2**layer_depth),
                                           batch_normalization=batch_normalization)
-        layer2 = create_convolution_block(input_layer=layer1, n_filters=n_base_filters*(2**layer_depth)*2,
+        layer2 = setup_block(input_layer=layer1, n_filters=n_base_filters*(2**layer_depth)*2,
                                           batch_normalization=batch_normalization)
         if layer_depth < depth - 1:
             current_layer = MaxPooling3D(pool_size=pool_size)(layer2)
@@ -79,12 +48,12 @@ def unet_model_3d(input_shape, pool_size=(2, 2, 2), n_labels=1, initial_learning
 
 
     for layer_depth in range(depth-2, -1, -1):
-        up_convolution = get_up_convolution(pool_size=pool_size, deconvolution=deconvolution,
+        up_convolution = do_convolution(pool_size=pool_size, deconvolution=deconvolution,
                                             n_filters=current_layer._keras_shape[1])(current_layer)
         concat = concatenate([up_convolution, levels[layer_depth][1]], axis=1)
-        current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
+        current_layer = setup_block(n_filters=levels[layer_depth][1]._keras_shape[1],
                                                  input_layer=concat, batch_normalization=batch_normalization)
-        current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
+        current_layer = setup_block(n_filters=levels[layer_depth][1]._keras_shape[1],
                                                  input_layer=current_layer,
                                                  batch_normalization=batch_normalization)
 
@@ -95,18 +64,42 @@ def unet_model_3d(input_shape, pool_size=(2, 2, 2), n_labels=1, initial_learning
     if not isinstance(metrics, list):
         metrics = [metrics]
 
-    if include_label_wise_dice_coefficients and n_labels > 1:
-        label_wise_dice_metrics = [get_label_dice_coefficient_function(index) for index in range(n_labels)]
+    if include_label_wise_coef_dices and n_labels > 1:
+        label_wise_dice_metrics = [get_coef_dice_function(index) for index in range(n_labels)]
         if metrics:
             metrics = metrics + label_wise_dice_metrics
         else:
             metrics = label_wise_dice_metrics
 
-    model.compile(optimizer=Adam(lr=initial_learning_rate), loss=dice_coefficient_loss, metrics=metrics)
+    model.compile(optimizer=Adam(lr=initial_learning_rate), loss=coef_dice_loss, metrics=metrics)
     return model
 
+def do_convolution(n_filters, pool_size, kernel_size=(2, 2, 2), strides=(2, 2, 2),
+                       deconvolution=False):
+    if deconvolution:
+        return Deconvolution3D(filters=n_filters, kernel_size=kernel_size,
+                               strides=strides)
+    else:
+        return UpSampling3D(size=pool_size)
 
-def create_convolution_block(input_layer, n_filters, batch_normalization=False, kernel=(3, 3, 3), activation=None,
+def get_coef_dice_function(label_index):
+    f = partial(label_wise_coef_dice, label_index=label_index)
+    f.__setattr__('__name__', 'label_{0}_dice_coef'.format(label_index))
+    return f
+
+
+dice_coef = coef_dice
+dice_coef_loss = coef_dice_loss
+
+K.set_image_data_format("channels_first")
+
+try:
+    from keras.engine import merge
+except ImportError:
+    from keras.layers.merge import concatenate
+
+
+def setup_block(input_layer, n_filters, batch_normalization=False, kernel=(3, 3, 3), activation=None,
                              padding='same', strides=(1, 1, 1), instance_normalization=False):
 
     layer = Conv3D(n_filters, kernel, padding=padding, strides=strides)(input_layer)
@@ -131,10 +124,4 @@ def compute_level_output_shape(n_filters, depth, pool_size, image_shape):
     return tuple([None, n_filters] + output_image_shape)
 
 
-def get_up_convolution(n_filters, pool_size, kernel_size=(2, 2, 2), strides=(2, 2, 2),
-                       deconvolution=False):
-    if deconvolution:
-        return Deconvolution3D(filters=n_filters, kernel_size=kernel_size,
-                               strides=strides)
-    else:
-        return UpSampling3D(size=pool_size)
+
